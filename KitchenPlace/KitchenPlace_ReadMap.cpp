57246919993a0645c51af4facf08fb254c9394fb
TheArquitect <44051807+TheArquitect@users.noreply.github.com>
2020-03-10 17:39:53 +0200

    Add files via upload

--- KitchenPlace/KitchenPlace_ReadMap.cpp
+++ KitchenPlace/KitchenPlace_ReadMap.cpp
@@ -5,11 +5,29 @@
 
     Read map from Kitchen.txt file
 
-    Menashe Rosemberg   Israel +972-52-323-0538
-    Copyright(c) 2019      All rights reserved.
+    BSD License
 
-    Software distributed under the MIT License is distributed on an "AS IS" BASIS,
-    NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit.
+    Copyright (c) 2019 TheArquitect (Menashe Rosemberg) rosemberg@ymail.com
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **/
 #include "KitchenPlace_ReadMap.h"
 #include "KitchenPlace_ERROR.h"
